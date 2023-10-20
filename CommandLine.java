d6cf83faa8ca390021893476179ee1f2272a9359
unknown <emma.broudic35@gmail.com>
2023-10-20 14:33:36 +0200

    input deux champs

--- CommandLine.java
+++ CommandLine.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class CommandLine {
+public class CommandLine {
 
     // Déclaration de deux champs de type String
     private String fieldOne;
@@ -23,7 +23,31 @@ class CommandLine {
         // Création d'une instance de la classe CommandLine
         CommandLine field = new CommandLine();
 
-        // Appel de la méthode pour afficher les champs
+        // Création d'un objet Scanner pour lire l'entrée de l'utilisateur
+        Scanner scanner = new Scanner(System.in);
+
+        // Demande à l'utilisateur de saisir une nouvelle valeur
+        System.out.print("Entrez une nouvelle valeur pour Champ 1 et Champ 2 : ");
+        String input = scanner.nextLine();
+
+        // Divise l'entrée en mots
+        String[] words = input.split(" ");
+
+        // Vérifie s'il y a au moins deux mots dans l'entrée
+        if (words.length >= 2) {
+            // Met à jour la valeur de fieldOne avec le premier mot
+            field.fieldOne = words[0];
+
+            // Met à jour la valeur de fieldTwo avec le deuxième mot
+            field.fieldTwo = words[1];
+        } else {
+            System.out.println("L'entrée doit contenir au moins deux mots pour mettre à jour Champ 1 et Champ 2.");
+        }
+
+        // Ferme le scanner
+        scanner.close();
+
+        // Appel de la méthode pour afficher les champs mis à jour
         field.printFields();
     }
 }
